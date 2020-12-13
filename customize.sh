665f956edfeba1afea60aa3705cdff684a865d68
hochenchong <30753404+hochenchong@users.noreply.github.com>
2020-12-13 14:02:27 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,2 +1,2 @@
 # 修改默认的 ip 地址
-sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
